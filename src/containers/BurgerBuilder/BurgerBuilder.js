09c70022a6735edeeeca62bf7563ed931d4400ec
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-09-20 01:25:43 +0000

    add Less button function

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,16 +34,37 @@ class BurgerBuilder extends Component {
     }
     
     removeIngredientHandler = (type) => {
-        
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const newPrice = this.state.totalPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
     }
     
     render () {
+        const disableInfo = {
+            ...this.state.ingredients
+        };
+        
+        for (let key in disableInfo) {
+            disableInfo[key] = disableInfo[key] <= 0;
+        }
+        
         return (
             <Aux>
                 <Burger ingredients = {this.state.ingredients} />
                 <div>Build controller</div>
                 <BuildControls 
-                    ingredientAdded = {this.addIngredientHandler}/>
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disableInfo}/>
             </Aux>
         );
     }
