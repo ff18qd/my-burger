be4656ba805c5ecf4c45ed0283a3ab8b766018c1
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-10-02 22:08:39 +0000

    add cancel and continue button to OrderSummary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -77,6 +77,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false});
     }
     
+    purchaseContinueHandler = () => {
+        alert('You Continue!');
+    }
+    
     render () {
         const disableInfo = {
             ...this.state.ingredients
@@ -89,7 +93,10 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCanceled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
                 <Burger ingredients = {this.state.ingredients} />
                 <div>Build controller</div>
